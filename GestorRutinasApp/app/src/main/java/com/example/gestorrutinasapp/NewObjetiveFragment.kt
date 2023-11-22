30307c487000b1ee311348103a56e08e36e66c7a
a21miguelsb <a21miguelsb@iessanclemente.net>
2023-11-22 17:54:26 +0100

    Añadiendo spinner fechas

--- GestorRutinasApp/app/src/main/java/com/example/gestorrutinasapp/NewObjetiveFragment.kt
+++ GestorRutinasApp/app/src/main/java/com/example/gestorrutinasapp/NewObjetiveFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TableLayout
 import android.widget.TableRow
 
@@ -17,22 +18,8 @@ class NewObjetiveFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_new_objetive, container, false)
 
-        val tableLayout = view.findViewById<TableLayout>(R.id.tableLayout)
-        val btnAddRow = view.findViewById<Button>(R.id.add_btn)
-
-        // Configurar el listener para el botón
-        btnAddRow.setOnClickListener {
-            val newRow = TableRow(requireContext())
-            // Add your views to the new row (e.g., EditText, TextView, etc.)
-            val editText = EditText(requireContext())
-            // Add other views as needed...
-
-            newRow.addView(editText)
-            // Add other views as needed...
-
-            // Add the new row to the table layout
-            tableLayout.addView(newRow)
-        }
+        val spinnerDays = view.findViewById<Spinner>(R.id.spinner_dias)
+        spinnerDays
 
         return view
     }
